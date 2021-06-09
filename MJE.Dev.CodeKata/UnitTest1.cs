dc29c55ab1f911c8c63493cb8d5880ae71829633
Mike Bowen <mike.bowen@bekagool.com>
2021-06-09 20:45:34 +0100

    Update UnitTest1.cs
    I borked it

--- MJE.Dev.CodeKata/UnitTest1.cs
+++ MJE.Dev.CodeKata/UnitTest1.cs
@@ -6,18 +6,16 @@ namespace MJE.Dev.CodeKata
 {
     public class StringCalculatorTests
     {
-
         [TestCaseSource(nameof(TestData))]
         public void Add_WithStringValues_ShouldReturnSum(string numberString, int expectedSum)
         {
-            var stringCalculator = new StringCalculator();
+            var res = StringCalculator.Add(numberString);
 
-            var res = stringCalculator.Add(numberString);
-
-            Assert.AreEqual(expectedSum,res);
+            Assert.AreEqual(expectedSum, res);
         }
 
-        public static object[] TestData = {
+        public static object[] TestData =
+        {
             new object[] {"", 0},
             new object[] {"1", 1},
             new object[] {"1,2", 3},
@@ -40,58 +38,87 @@ namespace MJE.Dev.CodeKata
         [TestCase("1,-1,3,-2", "-1,-2")]
         public void Add_WithNegativeStringValues_ShouldThrow(string numberString, string expectedValues)
         {
-            var stringCalculator = new StringCalculator();
-
             var expectedMessage = $"negatives not allowed {expectedValues}";
 
             try
             {
-                stringCalculator.Add("-1");
+                StringCalculator.Add(numberString);
             }
             catch (Exception e) when (e.Message.Equals(expectedMessage))
             {
                 Assert.Pass();
             }
-            catch 
+            catch
             {
                 Assert.Fail($"Expected Message : {expectedMessage} ");
             }
-
         }
 
+        [Test]
+        public void GetCalledCount_ReturnsAmountOfTimesAddCalled()
+        {
+            var stringCalculator = new StringCalculator();
 
+            for (var i = 0; i <= 3; i++)
+            {
+                var _ = StringCalculator.Add(i.ToString());
+            }
+
+            var res = stringCalculator.GetCalledCount();
+
+            Assert.AreEqual(3, res);
+        }
     }
 
     public class StringCalculator
     {
-        public int Add(string value)
+        public static int Add(string value)
         {
+            char[] GetDelims()
+            {
+                char delim = default;
+
+                if (value.StartsWith("//"))
+                {
+                    delim = value[2];
+
+                    value = value.Remove(0, 3);
+                }
+
+                var chars = delim != default ? new[] {delim} : new[] {',', '\n'};
+                return chars;
+            }
+
+            static void ValidateNumericValues(int[] ints)
+            {
+                var negativeNumericValues = ints.Where(x => x < 0).ToArray();
+
+                if (negativeNumericValues.Any())
+                {
+                    throw new Exception($"negatives not allowed {string.Join(",", negativeNumericValues)}");
+                }
+            }
+
             if (string.IsNullOrWhiteSpace(value))
             {
                 return 0;
             }
 
-            char delim = default;
+            var actualDelim = GetDelims();
 
-            if (value.StartsWith("//"))
-            {
-                delim = value[2];
+            var nums =
+                value
+                    .Split(actualDelim, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                    .Select(int.Parse).ToArray();
 
-                value = value.Remove(0, 3);
-            }
+            ValidateNumericValues(nums);
 
-            var actualDelim = delim != default ? new [] { delim } : new[] {',', '\n'};
+            return nums.Sum();
+        }
 
-            var nums = value.Split(actualDelim, StringSplitOptions.RemoveEmptyEntries|StringSplitOptions.TrimEntries);
-
-            var numericValues =  nums.Select(int.Parse).ToArray();
-
-            if (numericValues.Any(x=>x < 0))
-            {
-                throw new Exception($"negatives not allowed {numericValues.First(x=> x <0)}");
-            }
-            
-            return numericValues.Sum();
+        public int GetCalledCount()
+        {
+            return 0;
         }
     }
 }
