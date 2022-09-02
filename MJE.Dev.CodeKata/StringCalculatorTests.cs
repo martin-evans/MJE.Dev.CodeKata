be391a964277bb8ee13c658b86770281865500fd
Christa Bridges <christa@pebblepad.co.uk>
2022-09-02 10:48:20 +0100

    Refactored String calculator tests to use test cases and provided support for new line characters

--- MJE.Dev.CodeKata/StringCalculatorTests.cs
+++ MJE.Dev.CodeKata/StringCalculatorTests.cs
@@ -4,68 +4,27 @@ using System.Linq;
 
 namespace CCG.Dev.CodeKata
 {
-    public class StringCalculatorTests
-    {     
+    public class StringCalculatorTests {
 
-        [Test]
-        public void AddTakesEmptyStringReturnsZero()
-        {            
-            int sum = StringCalculator.Add("");
+        [TestCase("", 0)]
+        [TestCase("1", 1)]
+        [TestCase("1, 2", 3)]
+        [TestCase("18,24,4", 46)]
+        [TestCase("18,24,4,66", 112)]
+        [TestCase("1\n2,3", 6)]
+        public void AddReturnsSum(string csvString, int expectedResult) {
+            var sum = StringCalculator.Add(csvString);
 
-            Assert.AreEqual(0, sum);
+            Assert.AreEqual(expectedResult, sum);
         }
-
-        [Test]
-        public void AddTakesOneReturnsOne() {
-            
-            int sum = StringCalculator.Add("1");
-
-            Assert.AreEqual(1, sum);
-        }
-
-        [Test]
-        public void AddTakesCsvValueReturnsSum()
-        {
-            int sum = StringCalculator.Add("1,2");
-
-            Assert.AreEqual(3, sum);
-        }
-
-        [Test]
-        public void AddTakesRandomCsvValueReturnsSum() {
-
-            int sum = StringCalculator.Add("18,24,4");
-
-            Assert.AreEqual(46, sum);
-        }
-
-
-        [Test]
-        public void AddTakesAnotherRandomCsvValueReturnsSum()
-        {            
-            int sum = StringCalculator.Add("18,24,4,66");
-
-            Assert.AreEqual(112, sum);
-        }
-
-        [Test]
-        public void AddHAndlesNewLinesBetweenNumbers()
-        {
-
-            int sum = StringCalculator.Add("1\n2,3");
-
-            Assert.AreEqual(6, sum);
-
-        }
-
     }
 
     internal class StringCalculator
     {        
         static internal int Add(string numberString)
-        {            
+        {
             return string.IsNullOrEmpty(numberString) 
-                ? 0 : numberString.Split(",").Select(int.Parse).Sum();
+                ? 0 : numberString.Split(new string[] { "\n", "," }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Sum();
         }
     }
 }
